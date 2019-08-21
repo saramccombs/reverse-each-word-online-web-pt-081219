8c402f5d2855cd3102523c2d3506a53689c910f7
Sara McCombs <thesaramccombs@gmail.com>
2019-08-21 22:52:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,15 @@
 # method takes in a string that is a sentence
 # method returns the same sentence with each word reversed in place
 # 
-# 1. need to split string into array
-# 2. Reverse each word in place in the array
-# 3. return reversed string (rejoin array)
+# 1. need to reverse the string 
+# 2. need to split string into array
+# 3. Reverse the array 
+# 4. return reversed string via rejoining the array
 
 def reverse_each_word(sentence)
- sentence.reverse.split.reverse.join(" ")
+  sentence.reverse.split {|word| word.reverse}.join(" ") 
+ 
+ 
+ # method that doesn't use collect 
+ #sentence.reverse.split.reverse.join(" ")
 end
