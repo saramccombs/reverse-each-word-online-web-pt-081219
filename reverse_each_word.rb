be5c3b3a5992e45c6b9a8a470d128794e2aa8def
Sara McCombs <thesaramccombs@gmail.com>
2019-08-21 22:59:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,7 +9,7 @@
 
 def reverse_each_word(sentence)
  reverse_array = sentence.reverse.split
- reverse_array.reverse 
+ reverse_array.reverse.join
  
  # method that doesn't use collect 
  #sentence.reverse.split.reverse.join(" ")
