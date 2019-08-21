c65e619c28698f30462ce775b38b309a08c86307
Sara McCombs <thesaramccombs@gmail.com>
2019-08-21 22:33:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,12 @@
 # method named 'reverse_each_word
 # method takes in a string that is a sentence
 # method returns the same sentence with each word reversed in place
+# 
+# 1. need to split string into array
+# 2. Reverse each word in place in the array
+# 3. return reversed 
 
 def reverse_each_word(sentence)
-  sentence_array = sentence.split(" ")
-  reversed_sentence = ""
   
-  sentence_array.each do |word|
-    reversed_sentence + word.reverse 
-  end
   
 end
